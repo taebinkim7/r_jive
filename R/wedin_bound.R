d076e921ea3776302db1f8fab62d80d453930c59
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2023-04-21 16:00:42 -0400

    Check error

--- R/wedin_bound.R
+++ R/wedin_bound.R
@@ -12,6 +12,8 @@ get_wedin_bound_samples <- function(X, SVD, signal_rank, num_samples=1000){
 
     # resample for U and V
     U_perp <- SVD[['u']][ , -(1:signal_rank)]
+    print(dim(X))
+    print(dim(as.matrix(U_perp))
     U_sampled_norms <- wedin_bound_resampling(X=X,
                                               perp_basis=U_perp,
                                               right_vectors=FALSE,
@@ -44,9 +46,8 @@ wedin_bound_resampling <- function(X, perp_basis, right_vectors, num_samples=100
         sampled_col_index <- sample.int(n=n_dim,
                                         size=n_dim,
                                         replace=TRUE)
-
-
-        perp_resampled <- ifelse(is.null(dim(perp_basis)), as.matrix(perp_basis), perp_basis[ , sampled_col_index])
+                                        
+        perp_resampled <- ifelse(is.null(dim(perp_basis)), perp_basis, perp_basis[ , sampled_col_index])
 
         if(right_vectors){
             resampled_projection <- X %*% perp_resampled
